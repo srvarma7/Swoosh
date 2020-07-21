60ab5f1ac99cec7fe63670f55a38bd827dc76000
srvarma7 <srvarma7@gmail.com>
2020-07-21 16:14:17 +1000

    Adding segue using program from league to skills

--- Swoosh/Swoosh/Controller/LeagueVC.swift
+++ Swoosh/Swoosh/Controller/LeagueVC.swift
@@ -15,16 +15,11 @@ class LeagueVC: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Navigation to Skills VC
+    @IBAction func nextBtnTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
-    */
+    
 
 }
